2180fea2645d37e61f333517ef41d1df0bd21f66
magnus.coder <kelimbth@gmail.com>
2025-01-07 01:52:48 +0300

    homescreen

---
+++ lib/common/screens/homescreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:ride_share/screens/home/home.dart';
+import 'package:ride_share/screens/profile/profile_screen.dart';
+import 'package:ride_share/screens/rides/rides_screen.dart';
+import 'package:ride_share/utils/constants/colors.dart';
+import 'package:ride_share/widgets/custom_bottom_navbar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int screenIndex = 0;
+  final List<Widget> screens = [
+    Home(),
+    RidesScreen(),
+    Home(),
+    Home(),
+    ProfileScreen(),
+  ];
+
+  void moveToSelectedScreen(int index) {
+    setState(() {
+      screenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[screenIndex],
+      bottomNavigationBar: CustomBottomNavBar(
+        onTabClicked: (index) => moveToSelectedScreen(index),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          LineIcons.shareSquareAlt,
+          color: kIconLightColor,
+        ),
+      ),
+    );
+  }
+}
