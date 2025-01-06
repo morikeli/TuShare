a95b476af8c10056a76c26fbef4dc20f784daa59
magnus.coder <kelimbth@gmail.com>
2025-01-06 19:59:31 +0300

    colors used in the app

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kAppBarBackgroundColor = Color(0xFF2196F3);
+const kPrimaryColor = Color(0xFF1565C0);
+const kSecondaryColor = Color(0xFF111111);
+const kScaffoldBgColor = Color(0xFFF1EEEE);
+const kScaffoldDarkBgColor = Color(0xFF000000);
+const kIconLightColor = Color(0xFFFFFFFF);
+const kIconDarkColor = Color(0xFF000000);
+const kIconSecondaryColor = Color(0xFF757575);
+const kTabDividerLightColor = Color(0xFFF1EEEE);
+const kTabDividerDarkColor = Color(0xFF000000);
+const kTextLightColor = Color(0xFFFFFFFF);
+const kTextSecondaryColor = Color(0xFF78909C);
+const kTextDarkColor = Color(0xFF000000);
+const kUnselectedLabelColor = Color(0x60000000);
+const kCardLightColor = Color(0xFFFFFFFF);
+const kCardDarkColor = Color(0xFF111111);
+const kLabelSmallColor = Color(0xFF757575);
