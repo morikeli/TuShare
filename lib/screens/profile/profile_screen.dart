9faad51e2633f4e142edaadd1c4579e40b904215
magnus.coder <kelimbth@gmail.com>
2025-01-06 17:06:09 +0300

    created the profile screen

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:ride_share/screens/profile/components/profile_screen_body.dart';
+
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: ProfileScreenBody(),
+    );
+  }
+}
