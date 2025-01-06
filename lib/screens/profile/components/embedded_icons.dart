99ec1217289c3c16e13ccb9d2bd337e9bf4afae9
magnus.coder <kelimbth@gmail.com>
2025-01-06 19:54:32 +0300

    icon on user profile header section

---
+++ lib/screens/profile/components/embedded_icons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:ride_share/utils/constants/colors.dart';
+
+class EmbeddedIcons extends StatelessWidget {
+  const EmbeddedIcons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              LineIcons.horizontalEllipsis,
+              color: kIconLightColor,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
