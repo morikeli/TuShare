410f954fe9328d4d0c4e3f2aee64b3faaceef6a0
magnus.coder <kelimbth@gmail.com>
2025-01-14 18:44:28 +0300

    feature: toggle light/dark mode 🌞🌜

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:ride_share/common/widgets/icon_badge.dart';
 
 
 AppBar sharedAppBar(BuildContext context, String title, IconData? titleIcon) {
-    return AppBar(
-      centerTitle: true,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(titleIcon),
-          SizedBox(width: 5.0),
-          Text(title, style: Theme.of(context).textTheme.titleLarge),
-        ],
-      ),
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: Stack(
-            children: [Icon(LineIcons.bell), IconBadge()],
-          ),
-        ),
+  return AppBar(
+    centerTitle: true,
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(titleIcon),
+        SizedBox(width: 5.0),
+        Text(title, style: Theme.of(context).textTheme.titleLarge),
       ],
-    );
-  }
+    ),
+    actions: [
+      IconButton(
+        tooltip: 'Notifications',
+        onPressed: () {},
+        icon: Stack(
+          children: [Icon(LineIcons.bell), IconBadge()],
+        ),
+      ),
+      IconButton(
+        tooltip: 'Change theme',
+        onPressed: () {
+          if (Get.isDarkMode) {
+            Get.changeThemeMode(ThemeMode.light);
+          } else {
+            Get.changeThemeMode(ThemeMode.dark);
+          }
+        },
+        icon: Icon(Get.isDarkMode ? LineIcons.moon : LineIcons.sun),
+      ),
+    ],
+  );
+}
