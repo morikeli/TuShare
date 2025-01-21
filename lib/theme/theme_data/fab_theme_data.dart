4d3d6ca6697ff8dc0abca91cdf1a716d5339c92f
magnus.coder <kelimbth@gmail.com>
2025-01-21 15:42:12 +0300

    floating action button theme data 🟦

---
+++ lib/theme/theme_data/fab_theme_data.dart
@@ -0,0 +1,16 @@
+// Floating Action Button (FAB) theme data
+
+import 'package:flutter/material.dart';
+import 'package:ride_share/utils/constants/colors.dart';
+
+class TuShareAppFABTheme {
+  TuShareAppFABTheme._();
+
+  static FloatingActionButtonThemeData fabLightTheme = FloatingActionButtonThemeData(
+    backgroundColor: kPrimaryColor,
+  );
+
+  static FloatingActionButtonThemeData fabDarkTheme = FloatingActionButtonThemeData(
+    backgroundColor: kPrimaryColor,
+  );
+}
