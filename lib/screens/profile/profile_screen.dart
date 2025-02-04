9e0f38ce3196d1ce7ad3c26350929f87809f0fd8
magnus.coder <kelimbth@gmail.com>
2025-02-04 15:08:40 +0300

    updated import path

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ride_share/screens/profile/components/profile_screen_body.dart';
+import 'package:ride_share/screens/profile/components/profile_screen/profile_screen_body.dart';
 
 
 class ProfileScreen extends StatelessWidget {
