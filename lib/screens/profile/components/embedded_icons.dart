b48b9b2ba763316ead5df5f231be794c8f55643c
magnus.coder <kelimbth@gmail.com>
2025-02-04 13:08:27 +0300

    updated onPressed to navigate to edit profile screen

--- lib/screens/profile/components/embedded_icons.dart
+++ lib/screens/profile/components/embedded_icons.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:ride_share/screens/profile/edit_profile_screen.dart';
 import 'package:ride_share/utils/constants/colors.dart';
 
 class EmbeddedIcons extends StatelessWidget {
@@ -15,7 +17,11 @@ class EmbeddedIcons extends StatelessWidget {
         children: [
           IconButton(
             tooltip: 'Edit profile',
-            onPressed: () {},
+            onPressed: () => Get.to(
+              () => EditProfileScreen(),
+              transition: Transition.rightToLeft,
+              duration: Duration(milliseconds: 1500)
+            ),
             icon: Icon(
               LineIcons.userEdit,
               color: kIconLightColor,
