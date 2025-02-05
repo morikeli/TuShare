a9ea55b5ab9dec70bd0b69d35b84e8ab34d4ab54
magnus.coder <kelimbth@gmail.com>
2025-02-05 18:36:33 +0300

    1. added map pin to show the user their current position 2. used map controller to center the map using user's current location

--- lib/screens/slot/components/map_widget.dart
+++ lib/screens/slot/components/map_widget.dart
@@ -1,38 +1,84 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:ride_share/controllers/map_controller.dart';
 import 'package:ride_share/helpers/map_helper.dart';
 import 'package:ride_share/utils/constants/colors.dart';
-import 'package:universe/universe.dart';
+import 'package:universe/universe.dart' hide MapController;
 
 class MapWidget extends StatelessWidget {
-  const MapWidget({
-    super.key,
-  });
+  final MapController mapController = Get.put(MapController());
+
+  MapWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        U.OpenStreetMap(
-          center: [-6.175329, 106.827253],
-          type: OpenStreetMapType.HOT,
-          zoom: 15,
-        ),
-        Positioned(
-          top: 60,
-          right: 16,
-          child: IconButton.filled(
-            tooltip: 'View map in fullscreen',
-            style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll(Colors.white),
-              elevation: WidgetStatePropertyAll(15.0),
-              shadowColor: WidgetStatePropertyAll(kSecondaryColor),
-              shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)))
+    return Obx(() {
+      // display a loading indicator when the map is loading
+      if (mapController.isLoading.value) {
+        return Center(child: CircularProgressIndicator.adaptive());
+      }
+      // return map if its loaded
+      return Stack(
+        children: [
+          U.OpenStreetMap(
+            center: [
+              mapController.latitude.value,
+              mapController.longitude.value
+            ],
+            type: OpenStreetMapType.HOT,
+            zoom: 14,
+            markers: U.MarkerLayer(
+              data: 'The pin shows your current location',
+              [
+                U.Marker(
+                  [mapController.latitude.value, mapController.longitude.value],
+                  widget: MarkerIcon(
+                    icon: LineIcons.mapPin,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+              onTap: (latlng, data) {
+                Get.showSnackbar(GetSnackBar(
+                  backgroundColor: Colors.transparent,
+                  duration: Duration(seconds: 7),
+                  messageText: AwesomeSnackbarContent(
+                    title: 'You popped me!',
+                    message: '$data',
+                    messageTextStyle: TextStyle(fontSize: 16.0),
+                    contentType: ContentType.help,
+                  ),
+                ));
+              },
             ),
-            icon: Icon(Icons.fullscreen, color: kIconSecondaryColor),
-            onPressed: () => MapHelper.showFullScreenMap(context),
           ),
+          Positioned(
+            top: 60,
+            right: 16,
+            child: IconButton.filled(
+              tooltip: 'View map in fullscreen',
+              style: fullscreenButtonStyle(),
+              icon: Icon(Icons.fullscreen, color: kIconSecondaryColor),
+              onPressed: () => MapHelper.showFullScreenMap(context),
+            ),
+          ),
+        ],
+      );
+    });
+  }
+
+  ButtonStyle fullscreenButtonStyle() {
+    return ButtonStyle(
+      backgroundColor: WidgetStatePropertyAll(Colors.white),
+      elevation: WidgetStatePropertyAll(15.0),
+      shadowColor: WidgetStatePropertyAll(kSecondaryColor),
+      shape: WidgetStatePropertyAll(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
         ),
-      ],
+      ),
     );
   }
 }
