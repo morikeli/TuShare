671e406a058261f095117e55866a764f55d2918f
magnus.coder <kelimbth@gmail.com>
2025-02-18 16:04:38 +0300

    added new routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
 import 'package:ride_share/common/screens/homescreen.dart';
 import 'package:ride_share/screens/chats/chat_screen.dart';
+import 'package:ride_share/screens/login/login.dart';
+import 'package:ride_share/screens/signup/signup.dart';
 import 'package:ride_share/screens/slot/car_slot.dart';
 
 
 List<GetPage<dynamic>> routes = [
+  GetPage(name: '/login', page: () => LoginScreen()),
+  GetPage(name: '/signup', page: () => SignupScreen()),
   GetPage(name: '/home', page: () => HomeScreen()),
   GetPage(name: '/car-slot', page: () => CarSlot()),
   GetPage(name: '/chat', page: () => ChatScreen()),
