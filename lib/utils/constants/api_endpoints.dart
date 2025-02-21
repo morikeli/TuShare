2c5d174575e828566625b55f31f0bdd3b0e6d0b7
magnus.coder <kelimbth@gmail.com>
2025-02-21 14:45:37 +0300

    bug-fix: app can't receive response data due to missing forward slashe in endpoints 😵

--- lib/utils/constants/api_endpoints.dart
+++ lib/utils/constants/api_endpoints.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static String baseURL = dotenv.env['API_URL'] ?? 'http://localhost:8000/token/';
-  static String userProfile = '$baseURL/api/v1/users/profile/edit';
-  static String login = '$baseURL/api/v1/auth/login';
-  static String signup = '$baseURL/api/v1/auth/signup';
+  static String userProfile = '$baseURL/api/v1/users/profile/edit/';
+  static String login = '$baseURL/api/v1/auth/login/';
+  static String signup = '$baseURL/api/v1/auth/signup/';
 }
