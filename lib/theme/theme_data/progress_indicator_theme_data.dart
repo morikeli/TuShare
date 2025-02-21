f3c85e9db24e82e2aee95488557fdef21ebb53d5
magnus.coder <kelimbth@gmail.com>
2025-02-21 11:45:45 +0300

    theme data for prgress indicator

---
+++ lib/theme/theme_data/progress_indicator_theme_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:ride_share/utils/constants/colors.dart';
+
+
+class GoRideAppProgressIndicatorTheme {
+  GoRideAppProgressIndicatorTheme._();
+
+  static ProgressIndicatorThemeData progressIndicatorLightTheme = ProgressIndicatorThemeData(color: kPrimaryColor);
+
+  static ProgressIndicatorThemeData progressIndicatorDarkTheme = ProgressIndicatorThemeData(color: kPrimaryColor);
+  
+}
