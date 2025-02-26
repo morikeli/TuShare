77ea3720c9f965047de4fe649eadc077fb7f99f3
magnus.coder <kelimbth@gmail.com>
2025-02-26 12:48:48 +0300

    added a PopupMenuButton to display more options

--- lib/screens/profile/components/profile_screen/embedded_icons.dart
+++ lib/screens/profile/components/profile_screen/embedded_icons.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:ride_share/screens/profile/edit_profile_screen.dart';
-import 'package:ride_share/utils/constants/colors.dart';
+import 'package:ride_share/controllers/auth_controller.dart';
+
 
 class EmbeddedIcons extends StatelessWidget {
-  const EmbeddedIcons({
+  EmbeddedIcons({
     super.key,
   });
+
+  final AuthController authController = Get.put(AuthController());
 
   @override
   Widget build(BuildContext context) {
@@ -15,18 +17,44 @@ class EmbeddedIcons extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          IconButton(
-            tooltip: 'Edit profile',
-            onPressed: () => Get.to(
-              () => EditProfileScreen(),
-              transition: Transition.rightToLeft,
-              duration: Duration(milliseconds: 1500)
-            ),
-            icon: Icon(
-              LineIcons.userEdit,
-              color: kIconLightColor,
-            ),
-          )
+          PopupMenuButton(
+            onSelected: (value) {
+              if (value == 'edit_profile') {
+                Get.toNamed('/edit-profile');   // Navigate to Edit Profile screen
+              } else if (value == 'change_theme') {
+                if (Get.isDarkMode) {
+                  Get.changeThemeMode(ThemeMode.light);
+                } else {
+                  Get.changeThemeMode(ThemeMode.dark);
+                }
+              } else if (value == 'logout') {
+                authController.logout();
+              }
+            },
+            itemBuilder: (BuildContext context) => [
+              PopupMenuItem(
+                value: 'edit_profile',
+                child: ListTile(
+                  leading: const Icon(LineIcons.userEdit),
+                  title: const Text('Edit Profile'),
+                ),
+              ),
+              PopupMenuItem(
+                value: 'change_theme',
+                child: ListTile(
+                  leading: Icon(Get.isDarkMode ? LineIcons.moon : LineIcons.sun),
+                  title: const Text('Change Theme'),
+                ),
+              ),
+              PopupMenuItem(
+                value: 'logout',
+                child: ListTile(
+                  leading: const Icon(LineIcons.doorOpen, color: Colors.red),
+                  title: const Text('Logout'),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
