fb90a55ab7bfeda531b0eb871c89d2226150d779
magnus.coder <kelimbth@gmail.com>
2025-02-27 10:15:26 +0300

    updated API endpoints 🔁

--- lib/utils/constants/api_endpoints.dart
+++ lib/utils/constants/api_endpoints.dart
@@ -2,8 +2,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static String baseURL = dotenv.env['API_URL'] ?? 'http://localhost:8000/token/';
-  static String login = '$baseURL/api/v1/auth/login/';
+  static String login = '$baseURL/api/v1/auth/login';
   static String signup = '$baseURL/api/v1/auth/signup';
+  static String logout = '$baseURL/api/v1/auth/logout';
   static String editUserProfile = '$baseURL/api/v1/users/profile/edit';
   static String userProfile = '$baseURL/api/v1/users/profile';
   static String mediaURL = baseURL;
