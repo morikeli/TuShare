ee5ab07f85b04bc04e7b58b32006bde9c4323f14
magnus.coder <kelimbth@gmail.com>
2025-03-10 16:10:32 +0300

    updated code to fetch access token directly

--- lib/services/ride_service.dart
+++ lib/services/ride_service.dart
@@ -16,9 +16,9 @@ class RideService extends GetConnect  {
 
   // Fetches available rides for a given destination
   Future<List<Ride>> getAvailableRides(String destination) async {
-    final token = await getAccessToken();   // Fetch saved token
+    final token = await StorageService.getUserInfo();     // Fetch saved token
     final response = await get('${ApiConstants.getRides}?destination=$destination', headers: {
-      'Authorization': 'Bearer $token',
+      'Authorization': 'Bearer ${token["access_token"]}',
     });
 
     if (response.status.hasError) {
@@ -30,9 +30,9 @@ class RideService extends GetConnect  {
   
   // Fetches all rides booked by the current user
   Future<List<Ride>> getUserBookedRides() async {
-    final token = await getAccessToken();
+    final token = await StorageService.getUserInfo();
     final response = await get(ApiConstants.bookedRides, headers: {
-      'Authorization': 'Bearer $token',
+      'Authorization': 'Bearer ${token["access_token"]}',
     });
 
     if (response.status.hasError) {
@@ -44,9 +44,9 @@ class RideService extends GetConnect  {
 
   // Books a ride by sending a POST request
   Future<bool> bookRide(String rideId) async {
-    final token = await getAccessToken();
+    final token = await StorageService.getUserInfo();
     final response = await post('${ApiConstants.bookRide}/$rideId/book', {}, headers: {
-      'Authorization': 'Bearer $token',
+      'Authorization': 'Bearer ${token["access_token"]}',
     });
 
     return !response.status.hasError;
