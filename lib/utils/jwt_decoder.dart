664aa6bb047e18e715a254dc22a2b87da59f2d0a
magnus.coder <kelimbth@gmail.com>
2025-03-13 13:24:53 +0300

    created an object to decode jwt and return user id

---
+++ lib/utils/jwt_decoder.dart
@@ -0,0 +1,30 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:get/get.dart';
+import 'package:ride_share/services/storage_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class JwtDecoder {
+  static Future<String?> extractUserID() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('access_token');  // Retrieve access token
+
+    print('Token: $token');
+
+    if (token == null) {
+      await StorageService.clearUserInfo();     // delete previous access token
+      return Get.offNamed('/login');    // redirect user to the login screen
+    }
+    
+    final jwt = JWT.decode(token);
+    final userID = jwt.payload['user'];  // extract user ID in the token payload
+
+    if (userID == null) {
+      return Future.error("User ID not found in token.");
+    }
+
+    print('User ID: $userID');
+
+    return userID;
+  }
+}
