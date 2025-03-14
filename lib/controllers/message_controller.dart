490f7e46134a36b0b320687f1fc17fde93052704
magnus.coder <kelimbth@gmail.com>
2025-03-14 09:22:12 +0300

    created a message controller

---
+++ lib/controllers/message_controller.dart
@@ -0,0 +1,46 @@
+import 'package:get/get.dart';
+import 'package:ride_share/models/messages.dart';
+import 'package:ride_share/services/messages_service.dart';
+
+
+class MessagesController extends GetxController {
+  var isLoading = false.obs;
+  var currentUserMessages = <UserMessages>[].obs;
+  var groupChats = <ChatData>[].obs;
+  final MessagesService _messagesService = MessagesService();
+
+  @override
+  void onInit() {
+    fetchUserMessages();
+    super.onInit();
+  }
+
+
+  // method for retrieving group messages - shown in the message screen
+  Future<void> fetchUserMessages() async {
+    isLoading(true);
+    try {
+      var userMessages = await _messagesService.getUserMessages();
+      currentUserMessages.assignAll(userMessages);
+      update();     // update UI
+    } catch (e) {
+      throw Exception(e);
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  // method for retrieving all group chats
+  Future<void> fetchGroupChats(String rideID) async {
+    isLoading(true);
+   try {
+      var groupChatData = await _messagesService.getGroupChats(rideID);
+      groupChats.assignAll(groupChatData);
+      update();     // update UI
+    } catch (e) {
+      throw Exception(e);
+    } finally {
+      isLoading(false);
+    }
+  }
+}
