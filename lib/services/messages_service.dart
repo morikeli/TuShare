fef9b08cb5e7bae2a0a04688497992fde3293340
magnus.coder <kelimbth@gmail.com>
2025-03-14 09:21:02 +0300

    created a method to retrieve group chats

--- lib/services/messages_service.dart
+++ lib/services/messages_service.dart
@@ -42,5 +42,17 @@ class MessagesService extends GetConnect {
 
   }
 
-
+  // retrieve group chats of the selected group - selected in messages screen
+  Future<List<ChatData>> getGroupChats(String rideID) async {
+    final response  = await get('$url/$rideID/get');
+    
+    if (response.status.hasError) {
+      final Map<String, dynamic> responseBody = json.decode(response.body);
+      // add error message in responseMessage and show it in a snackbar.
+      final RxMap<String, dynamic> responseMessage = {"title": "Error", "message": "${responseBody['detail']}", "type": ContentType.failure}.obs;
+      return Future.error(CustomSnackbar(snackbarMessage: responseMessage));
+    } else {
+      return (response.body as List).map((messg) => ChatData.fromJson(messg)).toList();
+    }
+  }
 }
