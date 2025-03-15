810faa815086b9c2e2b5433a85faccb9f7e66765
magnus.coder <kelimbth@gmail.com>
2025-03-15 12:28:56 +0300

    created a method to send messages

--- lib/services/messages_service.dart
+++ lib/services/messages_service.dart
@@ -55,4 +55,24 @@ class MessagesService extends GetConnect {
       return (response.body as List).map((messg) => ChatData.fromJson(messg)).toList();
     }
   }
+
+  Future<bool> postMessage(rideID, messageContent) async {
+    String? userID = await JwtDecoder.extractUserID();  // Get user ID from token
+
+    final response = await post(ApiConstants.sendMessage, {
+      'sender_id': userID,
+      'ride_id': rideID,
+      'content': messageContent
+    });
+
+    if (response.status.hasError) {
+      final Map<String, dynamic> responseBody = json.decode(response.body);
+      // add error message in responseMessage and show it in a snackbar.
+      final RxMap<String, dynamic> responseMessage = {"title": "Error", "message": "${responseBody['detail']}", "type": ContentType.failure}.obs;
+      return Future.error(CustomSnackbar(snackbarMessage: responseMessage));
+    } else {
+      return !response.status.hasError;
+    }
+
+  }
 }
