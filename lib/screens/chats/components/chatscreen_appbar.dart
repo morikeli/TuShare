f5fe7933cb11f261c70dbf10134b6d40a88049f9
magnus.coder <kelimbth@gmail.com>
2025-03-16 13:02:50 +0300

    wrapped `Row()` with `GestureDetector()` widget to navigate to a new screen

--- lib/screens/chats/components/chatscreen_appbar.dart
+++ lib/screens/chats/components/chatscreen_appbar.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ride_share/screens/chats/group_members_screen.dart';
+import 'package:ride_share/utils/constants/api_endpoints.dart';
 import 'package:ride_share/utils/constants/colors.dart';
 
-
-AppBar chatScreenAppBar(BuildContext context) {
-    return AppBar(
-      title: Row(
+AppBar chatScreenAppBar(
+  BuildContext context,
+  String driverName,
+  String driverProfilePic,
+) {
+  return AppBar(
+    title: GestureDetector(
+      onTap: () {
+        Get.to(
+          duration: Duration(milliseconds: 1500),
+          transition: Transition.rightToLeftWithFade,
+          () => GroupMembersScreen(
+            driverName: driverName,
+            driverProfilePic: driverProfilePic,
+          ),
+        );
+      },
+      child: Row(
         children: [
           CircleAvatar(
             radius: 16.0,
-            backgroundImage: AssetImage('assets/images/dps/9.jpg'),
+            backgroundImage: NetworkImage(
+              '${ApiConstants.mediaURL}/$driverProfilePic',
+            ),
           ),
           SizedBox(width: 8.0),
           Text(
-            'Brenda jones',
+            driverName,
             style: TextStyle(
               color: kTextLightColor,
               fontSize: 16.0,
