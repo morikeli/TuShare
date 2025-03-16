90da573a5c93cd8c12ecabb1b4cf75839e12edb1
magnus.coder <kelimbth@gmail.com>
2025-03-16 13:07:50 +0300

    updated the ChatScreen widget

--- lib/screens/chats/chat_screen.dart
+++ lib/screens/chats/chat_screen.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ride_share/controllers/message_controller.dart';
 import 'package:ride_share/screens/chats/components/chatscreen_appbar.dart';
-import 'package:ride_share/screens/chats/components/receiver_chat_bubble.dart';
-import 'package:ride_share/screens/chats/components/send_message.dart';
-import 'package:ride_share/screens/chats/components/sender_chat_bubble.dart';
+import 'package:ride_share/screens/chats/components/chatscreen_body.dart';
 
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+  ChatScreen({
+    super.key,
+    required this.driverName,
+    required this.driverProfilePic,
+    required this.rideId
+  });
+
+  final String driverName, driverProfilePic, rideId;
+  final MessagesController messagesController = Get.put(MessagesController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: chatScreenAppBar(context),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4.0),
-        child: Column(
-          children: [
-            // chat bubbles
-            SenderChatBubble(),  // sender chat bubble
-            ReceiverChatBubble(),  // receiver chat bubble
-
-            Spacer(), // push textform field to the bottom of the screen - end of column
-            // text input - send message
-            SendMessageWidget()
-          ],
-        ),
-      ),
+      appBar: chatScreenAppBar(context, driverName, driverProfilePic),
+      body: ChatScreenBody(rideId: rideId),
     );
   }
 }
