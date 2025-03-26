14743017a0e9ed0b56fa461fc9561b74eb994d70
magnus.coder <kelimbth@gmail.com>
2025-03-26 12:14:13 +0300

    created a FAB to add new chats/messages

--- lib/screens/messages/messages_screen.dart
+++ lib/screens/messages/messages_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:ride_share/common/widgets/appbar.dart';
 import 'package:ride_share/screens/messages/components/message_screen_body.dart';
+import 'package:ride_share/utils/constants/colors.dart';
 
 
 class MessagesScreen extends StatelessWidget {
@@ -12,6 +13,13 @@ class MessagesScreen extends StatelessWidget {
     return Scaffold(
       appBar: sharedAppBar(context, 'Messages', LineIcons.comments),
       body: MessagesScreenBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          LineIcons.comment,
+          color: kIconLightColor,
+        ),
+      ),
     );
   }
 }
