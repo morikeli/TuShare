7d053648dad03f70d9cade9f268f3dc0453b0241
magnus.coder <kelimbth@gmail.com>
2025-03-28 08:23:06 +0300

    created a FAB to edit user profile

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:ride_share/screens/profile/components/profile_screen/profile_screen_body.dart';
+import 'package:ride_share/utils/constants/colors.dart';
 
 
 class ProfileScreen extends StatelessWidget {
@@ -7,8 +9,17 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: ProfileScreenBody(),
+    return Scaffold(
+      body: SafeArea(
+        child: ProfileScreenBody(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          LineIcons.userEdit,
+          color: kIconLightColor,
+        ),
+      ),
     );
   }
 }
