83ccb1900f9f83f9d8023a9317bd15fab12c6d17
magnus.coder <kelimbth@gmail.com>
2025-03-31 10:29:11 +0300

    1.) injected profile controller 2.) added tooltip 3.) updated onPressed()

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:ride_share/controllers/profile_controller.dart';
 import 'package:ride_share/screens/profile/components/profile_screen/profile_screen_body.dart';
 import 'package:ride_share/utils/constants/colors.dart';
 
-
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  ProfileScreen({super.key});
+
+  final ProfileController profileController = Get.put(ProfileController());
+
 
   @override
   Widget build(BuildContext context) {
+    final profile = profileController.userProfile.value;
+
     return Scaffold(
       body: SafeArea(
         child: ProfileScreenBody(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.toNamed('/edit-profile'),  // Navigate to Edit Profile screen,
+        tooltip: 'Edit profile',
+        onPressed: () {
+          if (profile == null) {
+            // do nothing
+          } else {
+            Get.toNamed('/edit-profile');
+          }
+        },  // Navigate to Edit Profile screen,
         child: Icon(
           LineIcons.userEdit,
           color: kIconLightColor,
